25e4706000f4762455c1b761d24eb741ad75ecfe
Carol Diane Hernandez <carol.d.hernandez@outlook.com>
2023-01-24 14:28:07 -0400

    Fixing output.

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 9):
-    for j in range(1, 10):
+    for j in range(i + 1, 10):
         print("{:d}".format(i), end='')
         if i == 8 and j == 9:
             print("{:d}".format(j), end='\n')
