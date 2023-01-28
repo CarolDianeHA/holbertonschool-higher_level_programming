638291493044ea98c89b96e2bae88c1d93fad66e
CarolDianeHA <carol.d.hernandez@outlook.com>
2023-01-28 11:34:05 -0400

    Function def magic_calculation(a, b, c): that does exactly the same as a Python bytecode.

--- python-if_else_loops_functions/102-magic_calculation.py
+++ python-if_else_loops_functions/102-magic_calculation.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def magic_calculation(a, b, c):
-    if (a < b) == False:
+    if not b < a:
         return (c)
-    elif (b > c) == False:
+    elif not b > c:
         return (a + b)
     else:
         return (a * b) - c
