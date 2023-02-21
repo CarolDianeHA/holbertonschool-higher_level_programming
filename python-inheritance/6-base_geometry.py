3b77dcf3c509f4b6ffcd182f694e6fa424f5d156
CarolDianeHA <carol.d.hernandez@outlook.com>
2023-02-20 20:52:01 -0400

    Raises an Exception with the message area() is not implemented.

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""BaseGeometry Class"""
+
+
+class BaseGeometry:
+    """BaseGeometry Class"""
+    def area(self):
+        Exception
+        return ("[Exeption] area() is not implemented")
