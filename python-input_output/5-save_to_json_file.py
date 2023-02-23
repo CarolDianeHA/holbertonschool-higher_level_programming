b0f4d8228e8b8f6bd9658b1284a8d11d05e42fe4
CarolDianeHA <carol.d.hernandez@outlook.com>
2023-02-23 19:32:53 -0400

    Changing the parameter to save json from x to w.

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -7,7 +7,7 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """_summary_"""
-    with open(filename, "x", encoding='utf-8') as file:
+    with open(filename, "w", encoding='utf-8') as file:
         text = json.dumps(my_obj)
         file.write(text)
     return len(text)
