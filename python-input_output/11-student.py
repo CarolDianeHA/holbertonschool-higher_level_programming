fa30a820e3f8f13e547e6bdac1c38ceff3eb1b4f
CarolDianeHA <carol.d.hernandez@outlook.com>
2023-02-23 20:14:01 -0400

    Function that replaces all attributes of the Student instance.

---
+++ python-input_output/11-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""_summary_"""
+
+
+class Student:
+
+    def __init__(self, first_name, last_name, age):
+        """_summary_"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        dic = {}
+        if (type(attrs) is not list):
+            return self.__dict__
+        else:
+            for i in attrs:
+                if (i in self.__dict__):
+                    dic[i] = self.__dict__[i]
+            return dic
+
+    def reload_from_json(self, json):
+        for i in json:
+            self.__dict__[i] = json[i]
