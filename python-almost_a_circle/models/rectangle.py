f59bcd25816483a3dcae0a7ee676cfabba62151b
CarolDianeHA <carol.d.hernandez@outlook.com>
2023-02-28 20:30:11 -0400

    Class Rectangle inherits from Base with width and height attributes.

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+"""Rectangle Class"""
+
+from models.base import Base
+
+class Rectangle(Base):
+    """Rectangle Class that inherits from Base"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Class Constructor"""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """Getter width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter width"""
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter height"""
+        self.__height = value
+
+
