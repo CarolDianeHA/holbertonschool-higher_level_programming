2533171ae890db12dd0eb332328ba624fc4ddb5e
CarolDianeHA <carol.d.hernandez@outlook.com>
2023-03-01 21:33:18 -0400

    Writes the JSON string representation of list_objs to a file.

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -3,6 +3,8 @@
 """Base Class"""
 
 import json
+import csv
+import os.path
 
 
 class Base:
@@ -25,3 +27,20 @@ class Base:
         if list_dictionaries is None:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes the JSON string representation of list_objs to a file"""
+        filename = "{}.json".format(cls.__name__)
+        list_dic = []
+
+        if list_objs is None:
+            pass
+        else:
+            for i in range(len(list_objs)):
+                list_dic.append(list_objs[i].to_dictionary())
+
+        lists = cls.to_json_string(list_dic)
+
+        with open(filename, 'w') as file:
+            file.write(lists)
