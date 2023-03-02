b88d5550bd66d988bcd892ace611d1f5b179af43
CarolDianeHA <carol.d.hernandez@outlook.com>
2023-03-01 21:49:43 -0400

    Returns an instance with all attributes already set.

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -51,3 +51,13 @@ class Base:
         if json_string is None:
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Returns an instance with all attributes already set"""
+        if cls.__name__ == "Rectangle":
+                new = cls(10, 10)
+        else:
+                new = cls(10)
+        new.update(**dictionary)
+        return new
