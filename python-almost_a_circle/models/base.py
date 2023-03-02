994e71b33662ec156909e648b0046fab635a719c
CarolDianeHA <carol.d.hernandez@outlook.com>
2023-03-01 21:41:19 -0400

    Returns the list of the JSON string representation.

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -49,5 +49,5 @@ class Base:
     def from_json_string(json_string):
         """Returns the list of the JSON string representation"""
         if json_string is None:
-            return []
-        return json.dumps(json_string)
+            return "[]"
+        return json.loads(json_string)
