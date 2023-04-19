952d3e336df380ad2d5b59b26e03b0d4d41a4db1
CarolDianeHA <carol.d.hernandez@outlook.com>
2023-04-18 20:11:19 -0400

    Script that creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,14 +1,18 @@
 -- Script that creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
 
-CREATE TABLE IF NOT EXIST second_table
+CREATE TABLE IF NOT EXISTS second_table
 (
-  id INT,
+  id INT AUTO_INCREMENT,
   name VARCHAR(256),
   score INT
 );
 INSERT INTO second_table
+(
+  name,
+  score
+)
 VALUE
-    ('1','John','10'),
-    ('2','Alex','3'),
-    ('3','Bob','14'),
-    ('4','George','8');
+    ('John','10'),
+    ('Alex','3'),
+    ('Bob','14'),
+    ('George','8');
