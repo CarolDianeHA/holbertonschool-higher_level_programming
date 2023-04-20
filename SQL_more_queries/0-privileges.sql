2cb9e9ea6b090c9101257134b322347d118dba38
CarolDianeHA <carol.d.hernandez@outlook.com>
2023-04-20 19:10:41 -0400

    Script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
 -- Script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
 
-SELECT *
-FROM localhost
-WHERE user IN ('user_0d_1', 'user_0d_2');
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
