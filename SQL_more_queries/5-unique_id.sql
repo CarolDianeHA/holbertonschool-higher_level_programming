948ceaceb9e523920ddd2cdce29b53457ae1b40f
CarolDianeHA <carol.d.hernandez@outlook.com>
2023-04-22 11:16:21 -0400

    Script that creates the table unique_id on your MySQL server.

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,7 +1,7 @@
 -- Script that creates the table unique_id on your MySQL server.
 
-REATE TABLE IF NOT EXISTS unique_id
+CREATE TABLE IF NOT EXISTS unique_id
 (
-  id INT DEFAULT 1,
+  id INT DEFAULT 1 AUTO_INCREMENT,
   name VARCHAR(256) NOT NULL
 );
