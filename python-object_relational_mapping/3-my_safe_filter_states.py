1550e8b19a7c581eeb6d9c770e727f3399c69427
CarolDianeHA <carol.d.hernandez@outlook.com>
2023-04-29 13:20:42 -0400

    Script that lists all states from the database hbtn_0e_0_usa.

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Script that lists all states from the database hbtn_0e_0_usa.
+
+With SQL injection.
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """Gather user input for MySQL credentials."""
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        user=user,
+        passwd=password,
+        db=db_name,
+        port=3306)
+
+    cursor = db.cursor()
+    querty = "SELECT * FROM states WHERE\
+        name = %s COLLATE utf8mb4_bin ORDER BY id ASC;"
+    cursor.execute(querty, (state,))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    db.close()
