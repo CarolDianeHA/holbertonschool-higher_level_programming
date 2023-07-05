294eabf15dd3e63a6aa6f0b0a6a0723849284e7d
CarolDianeHA <carol.d.hernandez@outlook.com>
2023-07-05 15:18:58 -0400

    7. I love C

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -2,10 +2,10 @@
 const number = process.argv[2];
 const phrase = 'C is fun';
 
-if (number){
-    for (let i = 0; i < number; i++){
-        console.log(phrase);
-    }
+if (number) {
+  for (let i = 0; i < number; i++) {
+    console.log(phrase);
+  }
 } else {
-    console.log("Missing number of occurrences")
+  console.log('Missing number of occurrences');
 }
