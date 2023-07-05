22e0089127a2e58a7b655f1f0a00c7ea3e1555f3
CarolDianeHA <carol.d.hernandez@outlook.com>
2023-07-05 15:04:44 -0400

    5. An Integer

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 const argument = process.argv[2];
-const number = parseInt(argument)
+const number = parseInt(argument);
 
-if (isNaN(number)){
-    console.log("Not a number");
-}else {
-    console.log("My number: " + number);
+if (isNaN(number)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + number);
 }
